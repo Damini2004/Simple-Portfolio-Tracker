cb57e0ac451fb632605a8890c387badd7c5861d8
Damini2004 <divyachachane8@gmail.com>
2024-12-30 20:01:38 +0530

    Commit 2

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',  // Ensure this is covering your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
